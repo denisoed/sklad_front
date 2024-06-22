4cf6eceaeae60593aede01a9c999b806851b6143
Denisoed <denisoed@gmal.com>
2024-06-22 13:09:18 +0600

    Test changes for disable html scroll

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,11 @@
 <template>
-  <div ref="appRef">
-    <router-view />
-  </div>
+  <router-view />
 </template>
 
 <script>
-import { defineComponent, onMounted, ref } from 'vue'
+import { defineComponent } from 'vue'
 
 export default defineComponent({
   name: 'App',
-  setup() {
-    const appRef = ref(null)
-
-    // Ensure the document is scrollable
-    function ensureDocumentIsScrollable() {
-      const isScrollable =
-        document.documentElement.scrollHeight > window.innerHeight;
-      // Check if the document is scrollable
-      console.log(isScrollable);
-      if (!isScrollable) {
-        /*
-        Set the document's height to 100 % of
-        the viewport height plus one extra pixel
-        to make it scrollable.
-        */
-        document.documentElement.style.setProperty(
-          "height",
-          "calc(100vh + 1px)",
-          "important"
-        );
-      }
-    }
-
-    function preventCollapse() {
-      if (window.scrollY === 0) {
-        window.scrollTo(0, 1);
-      }
-    }
-
-    onMounted(() => {
-      appRef.value.addEventListener('touchstart', preventCollapse);
-      ensureDocumentIsScrollable();
-    })
-
-    return {
-      appRef
-    }
-  }
 })
 </script>
--- src/boot/telegramAuth.js
+++ src/boot/telegramAuth.js
@@ -4,9 +4,9 @@ import useJwtMethods from 'src/modules/auth/useJwtMethods'
 export default boot(async () => {
   const telegram = window?.Telegram?.WebApp;
   if (telegram) {
-    telegram.isClosingConfirmationEnabled = true;
     telegram.expand()
     const { telegramAuth } = useJwtMethods()
     await telegramAuth()
+    telegram.ready()
   }
 })
