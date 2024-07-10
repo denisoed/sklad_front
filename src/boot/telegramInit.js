5cdb1ad69aaab3ea3f03b41d1c064961b46ed1cf
Denisoed <denisoed@gmal.com>
2024-07-10 22:58:24 +0600

    Added func insertInlineStyles

--- src/boot/telegramInit.js
+++ src/boot/telegramInit.js
@@ -1,9 +1,21 @@
 import { boot } from 'quasar/wrappers'
 import useJwtMethods from 'src/modules/auth/useJwtMethods'
 
+function insertInlineStyles() {
+  const style = document.createElement('style')
+  style.innerHTML = `
+    html {
+      height: 100vh;
+      overflow: hidden;
+    }
+  `.trim()
+  document.head.appendChild(style)
+}
+
 export default boot(async () => {
   const telegram = window?.Telegram?.WebApp;
   if (telegram?.initData) {
+    insertInlineStyles()
     telegram.expand()
     const { telegramAuth } = useJwtMethods()
     await telegramAuth(JSON.stringify(telegram.initDataUnsafe), 'tg')
