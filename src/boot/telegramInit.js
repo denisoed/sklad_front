1badb91d53b8d300aeda1e1eea9f7e3144e73974
Denisoed <denisoed@gmal.com>
2024-07-17 18:45:35 +0600

    Enable insertInlineStyles

--- src/boot/telegramInit.js
+++ src/boot/telegramInit.js
@@ -5,7 +5,7 @@ function insertInlineStyles() {
   const style = document.createElement('style')
   style.innerHTML = `
     html {
-      height: 100vh;
+      height: var(--tg-viewport-stable-height);
       overflow: hidden;
     }
     body {
@@ -18,7 +18,7 @@ function insertInlineStyles() {
 export default boot(async () => {
   const telegram = window?.Telegram?.WebApp;
   if (telegram?.initData) {
-    // insertInlineStyles()
+    insertInlineStyles()
     telegram.expand()
     const { telegramAuth } = useJwtMethods()
     await telegramAuth(JSON.stringify(telegram.initDataUnsafe), 'tg')
